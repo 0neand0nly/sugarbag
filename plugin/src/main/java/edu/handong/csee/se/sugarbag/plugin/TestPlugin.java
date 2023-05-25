cc25337e4c229a9a1789bb29f7858988d1cf9ba9
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2023-05-25 21:13:20 +0900

    feat: implement shouldInstrument

--- plugin/src/main/java/edu/handong/csee/se/sugarbag/plugin/TestPlugin.java
+++ plugin/src/main/java/edu/handong/csee/se/sugarbag/plugin/TestPlugin.java
@@ -1,12 +1,32 @@
 package edu.handong.csee.se.sugarbag.plugin;
 
-import com.sun.source.util.JavacTask;
-import com.sun.source.util.Plugin;
-import com.sun.source.util.TaskEvent;
-import com.sun.source.util.TaskListener;
+import com.google.errorprone.annotations.Var;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.util.*;
+import com.sun.tools.javac.api.BasicJavacTask;
+import com.sun.tools.javac.code.TargetType;
+import com.sun.tools.javac.code.TypeTag;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Name;
+import com.sun.tools.javac.util.Names;
+
+import javax.tools.JavaCompiler;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.sun.tools.javac.util.List.nil;
 
 public class TestPlugin implements Plugin {
     public static String NAME = "TestPlugin";
+
+    private static Set<String> TARGET_TYPES = new HashSet<>(Arrays.asList(
+            // Use only primitive types for simplicity
+            byte.class.getName(), short.class.getName(), char.class.getName(),
+            int.class.getName(), long.class.getName(), float.class.getName(), double.class.getName()));
 
     @Override
     public String getName() {
@@ -23,8 +43,25 @@ public class TestPlugin implements Plugin {
                     return;
                 }
 
-                System.out.println("hello plugin");
+                e.getCompilationUnit().accept(new TreeScanner<Void, Void>() {
+                    @Override
+                    public Void visitClass(ClassTree node, Void aVoid) {
+                        return super.visitClass(node, aVoid);
+                    }
+
+                    @Override
+                    public Void visitMethod(MethodTree node, Void aVoid) {
+                        return super.visitMethod(node, aVoid);
+                    }
+                }, null);
             }
         });
+    }
+
+    private boolean shouldInstrument(VariableTree parameter) {
+        return TARGET_TYPES.contains(parameter.getType().toString())
+                && parameter.getModifiers().getAnnotations()
+                .stream()
+                .anyMatch(a -> Positive.class.getSimpleName().equals(a.getAnnotationType().toString()));
     }
 }
