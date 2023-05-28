96212c22f67264ce9510d13484d979fb88583c8c
21700383 <21700383@handong.edu>
2023-05-28 21:51:16 +0900

    Plugin @Negative, @Neutral and the test files

---
+++ plugin/src/main/java/edu/handong/csee/se/sugarbag/plugin/Negative.java
@@ -0,0 +1,10 @@
+package edu.handong.csee.se.sugarbag.plugin;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.PARAMETER})
+public @interface Negative {
+}
+
